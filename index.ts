042a5625da463933586328ee24359956e68e2640
martinavelkovikj-metergram <martina.velkovikj@metergram.com>
2023-06-26 19:46:08 +0200

    Fixing database bug

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import { AppSource } from "./util/database"
 import express from "express";
 import { insertMoviesData } from "./insertMoviesToDB";
+import{ Movie } from "./model/Movie" 
+import { router } from "./routes/movie";
+
 
 const app = express();
 app.use(express.json());
+app.use(router);
 
 AppSource.initialize()
-.then(() => {
-    
+.then(async () => {
+    await Movie.clear();
+
     app.listen(3000, () => {
         console.log("Server is running on port 3000");
     })
